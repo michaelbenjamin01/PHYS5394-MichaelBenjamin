473b5a862a22e9c7cfbb71d136c84a25119523cd
michaelbenjamin01 <97982489+michaelbenjamin01@users.noreply.github.com>
2022-03-07 14:48:37 -0600

    L6Lab submission

---
+++ L6Lab/testexpdampsin.m
@@ -0,0 +1,110 @@
+clear
+clc
+
+addpath ../L3Lab;
+%% Exponentially damped sinusoid
+% This function is windowed for t in the range [t_a, t_a + L], everywhere else it is
+% zero. Because of this, we will only be finding the sampling frequency for
+% the function in the window [t_a, t_a + L].
+
+% signal has the form s(t) = a(t)*cos(2*pi*phi(t)) with the following:
+% a(t) = A*exp(-(t-t_a)/tau)
+% phi(t) = (f_0 * t + phi_0/(2pi))
+
+% f(t) = d(phi)/dt = f_0
+% f_0 is constant throughout the signal so the nyquist rate is 2*f_0.
+
+% parameters
+SNR = 10; % amplitude of our signal
+ta =  1; % beginning of the window for the exponentially damped sinusoid
+f0 = 5; % initial frequency as discussed in the above description, f0  
+tau = 2;
+phi0 = 5; % initial phase
+L = pi; % end of the window for exponentially damped sinusoid
+nqstfs = 2*f0; % nyquist sampling frequency
+
+%% plotting for sampling frequency as 5 times nyquist sampling frequency.
+
+fs = 5 * nqstfs; % sampling frequency
+samplIntvl = 1/fs; % sampling interval;
+timeVec = 0:samplIntvl:5;
+
+% generating signal
+sigVec = expdampsin(timeVec,SNR,ta,f0,tau,phi0,L);
+
+% plotting signal
+figure;
+plot(timeVec,sigVec);
+title('exponentially damped sinusoid timeseries, sampling frequency fs = 5*nqstfs')
+xlabel('time');
+ylabel('amplitude');
+grid on; grid minor;
+
+% plotting periodogram
+dataLen = timeVec(end) - timeVec(1); % obtaining length of data
+kNyq = floor(length(timeVec)/2)+1; % DFT point at nyquist frequency
+posFreq = (0:(kNyq-1))*(1/dataLen); % positive frequencies in fft
+fftSig = fft(sigVec); % fast fourier transform of signal
+fftSig = fftSig(1:kNyq); % removing frequencies to the left of nyquist freq
+
+%FIXME Add axes labels 
+figure;
+plot(posFreq,abs(fftSig));
+title('exponentially damped sinusoid periodogram, sampling frequency fs = 5*nqstfs');
+xlabel('Positive Frequencies');
+ylabel('Amplitude');
+grid on; grid minor;
+
+% plotting spectrogram
+winLen = 0.3; % in seconds
+ovrlp = 0.1; % in seconds
+
+winLenSmpls = floor(winLen*fs);
+ovrlpSmpls = floor(ovrlp*fs);
+[S,F,T]=spectrogram(sigVec,winLenSmpls,ovrlpSmpls,[],fs);
+figure;
+imagesc(T,F,abs(S)); axis xy;
+title('exponentially damped sinusoid spectrogram, sampling frequency fs = 5*nqstfs');
+xlabel('Time (sec)');
+ylabel('Frequency (Hz)');
+
+%% plotting for sampling frequency as 1/2 of nyquist sampling frequency.
+
+fs = 0.5 * nqstfs; % sampling frequency
+samplIntvl = 1/fs; % sampling interval;
+timeVec = 0:samplIntvl:5;
+
+% generating signal
+sigVec = expdampsin(timeVec,SNR,ta,f0,tau,phi0,L);
+
+% plotting signal
+figure;
+plot(timeVec,sigVec);
+title('exponentially damped sinusoid timeseries, sampling frequency fs = 1/2*nqstfs')
+grid on; grid minor;
+
+% plotting periodogram
+dataLen = timeVec(end) - timeVec(1); % obtaining length of data
+kNyq = floor(length(timeVec)/2)+1; % DFT point at nyquist frequency
+posFreq = (0:(kNyq-1))*(1/dataLen); % positive frequencies in fft
+fftSig = fft(sigVec); % fast fourier transform of signal
+fftSig = fftSig(1:kNyq); % removing frequencies to the left of nyquist freq
+
+%FIXME Add axes labels 
+figure;
+plot(posFreq,abs(fftSig));
+title('exponentially damped sinusoid periodogram, sampling frequency fs = 1/2*nqstfs')
+grid on; grid minor;
+
+% plotting spectrogram
+winLen = 0.4; % in seconds
+ovrlp = 0.3; % in seconds
+
+winLenSmpls = floor(winLen*fs);
+ovrlpSmpls = floor(ovrlp*fs);
+[S,F,T]=spectrogram(sigVec,winLenSmpls,ovrlpSmpls,[],fs);
+figure;
+imagesc(T,F,abs(S)); axis xy;
+title('exponentially damped sinusoid spectrogram, sampling frequency fs = 1/2*nqstfs');
+xlabel('Time (sec)');
+ylabel('Frequency (Hz)');
---
+++ L6Lab/teststepFM.m
@@ -0,0 +1,107 @@
+clear
+clc
+
+addpath ..\L3Lab
+%% stepFM(data,A,ta,f0,f1)
+% This function has two different forms depending if t =< t_a or if t >
+% t_a. As a result, we will find the nyquist sampling frequency for both
+% conditions.
+
+% for t =< t_a:
+% s(t) = A*sin(2*pi*f_0*t)
+% This means that phi(t) = f_0 * t, and d(phi)/dt = f_0.
+% This means nyquist sampling frequency is 2*f_0.
+
+% for t > t_a:
+% s(t) = A*sin(2*pi*f_1*(t-t_a) + 2*pi*f_0*t_a)
+% so, phi(t) = f_1*(t-t_a) + f_0*t_a.
+% then, d(phi)/dt = f_1 and nyquist sampling frequency is 2*f_1.
+
+% parameters
+SNR = 10;
+ta = 1;
+f0 = 10;
+f1 = 10;
+nqstfs = 2*f0; % Nyquist sampling frequency
+
+%% plotting for sampling frequency as 5 times nyquist sampling frequency.
+
+fs = 5 * nqstfs; % sampling frequency
+samplIntvl = 1/fs; % sampling interval;
+timeVec = 0:samplIntvl:5;
+% generating signal
+sigVec = stepFM(timeVec,SNR,ta,f0,f1);
+
+% plotting signal
+figure;
+plot(timeVec,sigVec);
+title('step FM timeseries, sampling frequency fs = 5*nqstfs')
+grid on; grid minor;
+
+% plotting periodogram
+dataLen = timeVec(end) - timeVec(1);
+kNyq = floor(length(timeVec)/2)+1;
+posFreq = (0:(kNyq-1))*(1/dataLen);
+fftSig = fft(sigVec);
+fftSig = fftSig(1:kNyq);
+
+figure;
+plot(posFreq,abs(fftSig));
+title('step FM periodogram, sampling frequency fs = 5*nqstfs');
+grid on; grid minor;
+
+% plotting spectrogram
+winLen = 0.4; % in seconds
+ovrlp = 0.01; % in seconds
+
+winLenSmpls = floor(winLen*fs);
+ovrlpSmpls = floor(ovrlp*fs);
+[S,F,T]=spectrogram(sigVec,winLenSmpls,ovrlpSmpls,[],fs);
+figure;
+imagesc(T,F,abs(S)); axis xy;
+title('step FM spectrogram, sampling frequency fs = 5*nqstfs');
+xlabel('Time (sec)');
+ylabel('Frequency (Hz)');
+
+%% plotting for sampling frequency as 1/2 of the nyquist sampling frequency
+
+fs = 0.5 * nqstfs; % sampling frequency
+samplIntvl = 1/fs; % sampling interval;
+timeVec = 0:samplIntvl:5;
+% generating signal
+sigVec = stepFM(timeVec,SNR,ta,f0,f1);
+
+% plotting signal
+figure;
+plot(timeVec,sigVec);
+xlabel('time (s)');
+ylabel('Amplitude');
+title('step FM timeseries, sampling frequency fs = 1/2*nqstfs')
+grid on; grid minor;
+
+% plotting periodogram
+dataLen = timeVec(end) - timeVec(1);
+kNyq = floor(length(timeVec)/2)+1;
+posFreq = (0:(kNyq-1))*(1/dataLen);
+fftSig = fft(sigVec);
+fftSig = fftSig(1:kNyq);
+
+figure;
+plot(posFreq,abs(fftSig));
+xlabel('frequency (Hz)');
+ylabel('Power');
+title('step FM periodogram, sampling frequency fs = 1/2*nqstfs');
+grid on; grid minor;
+
+% plotting spectrogram
+winLen = 2.0; % in seconds
+ovrlp = 1.9; % in seconds
+
+winLenSmpls = floor(winLen*fs);
+ovrlpSmpls = floor(ovrlp*fs);
+[S,F,T]=spectrogram(sigVec,winLenSmpls,ovrlpSmpls,[],fs);
+figure;
+imagesc(T,F,abs(S)); axis xy;
+title('step FM spectrogram, sampling frequency fs = 1/2*nqstfs');
+xlabel('Time (sec)');
+ylabel('Frequency (Hz)');
