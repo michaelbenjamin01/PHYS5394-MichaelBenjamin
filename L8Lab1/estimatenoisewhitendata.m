b081255db20d65d2c52af1d92ffdede02808a72f
soumya <soumya.d.mohanty@gmail.com>
2022-04-12 19:41:08 -0500

    SDM comments and fixes.

--- L8Lab1/estimatenoisewhitendata.m
+++ L8Lab1/estimatenoisewhitendata.m
@@ -24,26 +24,30 @@ plot(f1,pxx1);
 title('PSD of noise from data')
 
 %% Modelling target PSD
-
+%FIXME Incorrect1: Review the tasks list carefully. No model PSD was needed.
+%list
 targetPSD = @(f) (f>=100 & f<=300).*(f-100).*(300-f)/1600000;
 
 % plot target PSD
-
-freqVec = 0:0.1:512;
-psdVec = targetPSD(freqVec);
-figure;
-hold on;
-plot(freqVec,psdVec);
-plot(f1,pxx1);
-legend('target PSD','noise PSD');
-title('target PSD overlapped with noise psd');
+%FIXME This figure is not connected to the lab
+% freqVec = 0:0.1:512;
+% psdVec = targetPSD(freqVec);
+% figure;
+% hold on;
+% plot(freqVec,psdVec);
+% plot(f1,pxx1);
+% legend('target PSD','noise PSD');
+% title('target PSD overlapped with noise psd');
 
 %% Designing filter and passing data through it
 
 filtrOrdr = 500;
-
-sqrtPSD = sqrt(psdVec);
-b = fir2(filtrOrdr,freqVec/(sampFreq/2),sqrtPSD);
+%FIXME Incorrect2: You had to estimate the PSD from the noise part
+%sqrtPSD = sqrt(psdVec);
+sqrtPSD = sqrt(pxx1);
+%%FIXME Incorrect3: Whitening filter transfer function is 1/sqrt(PSD)
+%b = fir2(filtrOrdr,freqVec/(sampFreq/2),sqrtPSD);
+b = fir2(filtrOrdr,f1/(sampFreq/2),1./sqrt(pxx1));
 outData = sqrt(sampFreq)*fftfilt(b,testData(:,2));
 
 [pxx2,f2] = pwelch(outData, 256, [], [], sampFreq);
@@ -56,5 +60,16 @@ plot(f2, pxx2);
 title('before and after whitening of data');
 legend('data before whitening','data after whitening');
 
+%FIXME Lab instructions required spectrograms and time series plots
+%SDM Add titles, axes labels etc.
+figure;
+subplot(2,1,1);
+[S,F,T] = spectrogram(testData(:,2),64,[],[],sampFreq);
+imagesc(T,F,abs(S)); axis xy;
+subplot(2,1,2);
+[S,F,T] = spectrogram(outData,64,[],[],sampFreq);
+imagesc(T,F,abs(S)); axis xy;
+
+%FIXME Your conclusion was not supported by your original code and results.
 % You can see very clearly that the power of the signal is increased after
 % whitening, and the shape of the signal is also siginficantly improved.
